89117cbc9766043b47b4bb9dd1c47c0ed3d8e0f5
Pavel Gabriel <alovak@gmail.com>
2011-10-17 11:07:30 +0300

    prepare target db tables for merge

--- db.thor
+++ db.thor
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
 require 'merge_db'
 
 class Db < Thor
+  include MergeDb
   desc "merge", "merge two databases"
   method_option :source, :aliases => "-s", :required => true, :desc => "Records will be copied from this database"
   method_option :target, :aliases => "-t", :required => true, :desc => "Records will be inserted into this database"
--- lib/merge_db.rb
+++ lib/merge_db.rb
@@ -21,19 +21,65 @@ module MergeDb
   class Target < ActiveRecord::Base
   end
 
-  def self.merge(source, target)
-    Source.establish_connection(Configuration.database[source])
-    Target.establish_connection(Configuration.database[target])
+  class << self
+    def merge(source, target)
+      Source.establish_connection(Configuration.database[source])
+      Target.establish_connection(Configuration.database[target])
 
-    Source.connection.tables.each do |table|
-      query = "select * from #{table}"
+      prepare_tables_in_target
+      copy_data_from_source_to_target
+      update_association_ids
 
-      Source.connection.select_all(query).each do |fixture|
-        fixture.delete("id")
-        Target.connection.insert_fixture(fixture, table)
+      puts "Databases were merged."
+    end
+
+    private
+
+    def prepare_tables_in_target
+      Target.connection.tables.each do |table|
+        Target.connection.columns(table).each do |column|
+          if column.name =~ /id$/
+            Target.connection.add_column(table, new_column_name(column.name), column.type)  
+          end
+        end
       end
     end
 
-    puts "Databases were merged."
+    def copy_data_from_source_to_target
+      Source.connection.tables.each do |table|
+        query = "select * from #{table}"
+
+        Source.connection.select_all(query).each do |fixture|
+          fixture_with_saved_ids = prepare_for_merge(fixture)
+          Target.connection.insert_fixture(fixture_with_saved_ids, table)
+        end
+      end
+    end
+
+    def update_association_ids
+      # Update references
+      # for each table go through __id columns
+      # find __id of the association
+      # get id
+      # set association_id
+    end
+
+    def prepare_for_merge(fixture)
+      {}.tap do |new_fixture|
+        fixture.each do |key, value|
+          next if key == "id"
+
+          new_fixture[new_column_name(key)] = value
+        end
+      end
+    end
+
+    def new_column_name(name)
+      if name =~ /id$/
+        name = name.gsub(/id$/, "_id")
+      else
+        name
+      end
+    end
   end
 end
