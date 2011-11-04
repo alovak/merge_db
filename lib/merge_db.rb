96a570cf76ef1820db81b0184ef7c7145be26411
Pavel Gabriel <alovak@gmail.com>
2011-11-04 21:58:06 +0200

    check that config/merge exists

--- lib/merge_db.rb
+++ lib/merge_db.rb
@@ -4,11 +4,26 @@ require "bundler/setup"
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'active_record'
-# require 'ruby-debug'
 require 'logger'
 require 'progressbar'
 
 require 'merge_db/configuration'
+
+begin
+  require './config/merge'
+rescue LoadError
+  puts <<-XML
+
+  Warning!!!
+
+  Please, create ./config/merge.rb file with the following content:
+
+    MergeDb::Configuration.scope_name = 'studio' #singular name of table
+    MergeDb::Configuration.scoped_tables = %w(users groups) #tables that should be scoped
+  XML
+  exit 1
+end
+
 require 'merge_db/merger'
 
 ActiveRecord::Base.logger = Logger.new('log.txt')
@@ -16,4 +31,3 @@ ActiveRecord::Base.logger = Logger.new('log.txt')
 module MergeDb
 end
 
-require "./config/merge.rb"
